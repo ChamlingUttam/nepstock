71cb22cca0500da00b8a95b99ac631705b2f593d
Lija <leeza9797@gmail.com>
2026-09-21 13:06:45 +0545

    feat: Updated the buttons in the Navbar

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
 
   return (
     <nav className="mt-5 h-[70px] w-full px-4 md:px-12">
-      <div className="mx-auto flex h-full max-w-7xl items-center justify-between">
+      <div className="relative mx-auto flex h-full max-w-7xl items-center justify-between">
         
         {/* Logo */}
         <div className="flex items-center">
@@ -27,14 +27,14 @@ export default function Navbar() {
           </Link>
         </div>
 
-        {/* Navigation Links */}
-        <div>
-          <ul className="flex items-center gap-2.5 text-[18px] font-normal leading-7 tracking-[-0.03em] md:gap-4 lg:gap-8">
+        {/* Center Navigation */}
+        <div className="absolute left-1/2 -translate-x-1/2">
+          <ul className="flex items-center gap-8">
             {navItems.map((item) => (
               <li key={item.name}>
                 <Link
                   href={item.href}
-                  className="cursor-pointer text-[#475569] transition-colors hover:text-gray-400"
+                  className="cursor-pointer whitespace-nowrap text-[18px] font-normal leading-7 tracking-[-0.03em] text-[#475569] transition-colors hover:text-gray-400"
                 >
                   {item.name}
                 </Link>
@@ -43,23 +43,26 @@ export default function Navbar() {
           </ul>
         </div>
 
-        {/* Buttons */}
-        <div className="hidden items-center gap-3 md:flex">
+        {/* Right Buttons */}
+        <div className="ml-auto flex items-center gap-3">
           <Link href="/login">
             <Button
               variant="ghost"
-              className="cursor-pointer border border-gray-100 font-semibold text-black outline"
+              className="h-auto cursor-pointer border border-gray-200 bg-white px-4 py-3 font-poppins text-[16px] font-semibold uppercase leading-4 tracking-[-0.01em] text-[#475569] outline-none hover:bg-white hover:text-[#475569]"
             >
               LOG IN
             </Button>
           </Link>
 
           <Link href="/get-started">
-            <Button className="cursor-pointer bg-[#007BFF] font-bold text-white hover:bg-[#085bb4]">
+            <Button
+              className="h-auto cursor-pointer bg-[#007BFF] px-5 py-3 font-poppins text-[16px] font-semibold uppercase leading-4 tracking-[-0.01em] text-white hover:bg-[#085bb4]"
+            >
               START FREE TRIAL
             </Button>
           </Link>
         </div>
+
       </div>
     </nav>
   );
